3a816de501728c7d2298cf469a7a118deddd681d
laughingsolo <laughingsolo@laughingsolos-MacBook-Pro.local>
2018-04-09 15:52:07 +0800

    toupiao

---
+++ polls/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
+from django.urls import reverse
+from .models import Question, Choice
+from django.views import generic
+
+# Create your views here.
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # 重新显示该问题的表单
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # 始终在成功处理 POST 数据后返回一个 HttpResponseRedirect ，
+        # （合并上句） 这样可以防止用户点击“后退”按钮时数据被发送两次。
+        # （合并至上一句）
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
